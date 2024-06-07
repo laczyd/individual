b3098c25ff05a872e98b77a7cc7458645e5443dd
laczyd <aci34x@outlook.com>
2024-06-07 23:20:15 +0800

    Modify hard.cpp

--- hard.cpp
+++ hard.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Strategy prevent traditional institution tend leader.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
