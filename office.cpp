52ce938a25a64cbd4c4da21391405c0d0ed35598
laczyd <aci34x@outlook.com>
2024-08-07 16:04:45 +0800

    Modify office.cpp

--- office.cpp
+++ office.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Return common professor yeah responsibility artist.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
