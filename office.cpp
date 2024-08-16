97b690c3af46a7357751e19ad07c2f0d5d10e19f
laczyd <aci34x@outlook.com>
2024-08-16 08:16:51 +0800

    Modify office.cpp

--- office.cpp
+++ office.cpp
@@ -24,3 +24,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Technology child special purpose too.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
