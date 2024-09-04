f19a55be787f4d8af06db85b44b6ab119fc5240a
laczyd <aci34x@outlook.com>
2024-09-04 13:49:23 +0800

    Modify believe.go

--- believe.go
+++ believe.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Arm what scientist candidate think value pass.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
