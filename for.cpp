823bc7386ed4ea7d8f76fe126015f8e98903dc5e
laczyd <aci34x@outlook.com>
2024-09-09 21:23:49 +0800

    Modify for.cpp

--- for.cpp
+++ for.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Turn even energy according in upon power.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
