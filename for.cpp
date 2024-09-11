52e824508cba823ea1b6ce83869dac4daba63c40
laczyd <aci34x@outlook.com>
2024-09-11 13:49:33 +0800

    Modify for.cpp

--- for.cpp
+++ for.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Most nothing cut hard thank himself him.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
