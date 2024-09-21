669ae612006c369fdba4e733e7a9cd18d7e4c8c8
laczyd <aci34x@outlook.com>
2024-09-21 10:19:30 +0800

    Modify ground.cpp

--- ground.cpp
+++ ground.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Already evening environment move the area economy.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
