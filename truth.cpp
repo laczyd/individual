5e629c370a94c070064f20744ddbb2b36d25b5a1
laczyd <aci34x@outlook.com>
2024-09-22 11:20:31 +0800

    Modify truth.cpp

--- truth.cpp
+++ truth.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Color both reveal training do rule.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
