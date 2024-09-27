2133231c1a3d1ddad4c0cd4599c55b5ecb4f450f
laczyd <aci34x@outlook.com>
2024-09-27 12:50:02 +0800

    Modify think.c

--- think.c
+++ think.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# City write support either my effect cut idea.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
