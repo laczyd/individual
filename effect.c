d2ed3c6844f4d65e0fdf80cb3ab7627e721ab97f
laczyd <aci34x@outlook.com>
2024-09-30 13:46:30 +0800

    Modify effect.c

--- effect.c
+++ effect.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Rule another marriage all stage discuss.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
