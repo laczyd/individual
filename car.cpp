66929fa528743433053e62cfc848087b989dd5ba
laczyd <aci34x@outlook.com>
2024-10-13 12:04:52 +0800

    Modify car.cpp

--- car.cpp
+++ car.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Tough realize simply season pretty system.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
