575fbae4ffc7739718673cffa36f92e24d45e7ed
laczyd <aci34x@outlook.com>
2024-11-25 16:44:47 +0800

    Modify professor.cpp

--- professor.cpp
+++ professor.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Fine suddenly recent political.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
