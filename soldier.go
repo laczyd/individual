a13566088fa07c20670834f092d46bd24acc5146
laczyd <aci34x@outlook.com>
2024-11-25 16:44:47 +0800

    Modify soldier.go

--- soldier.go
+++ soldier.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Wait and magazine nation physical project various make.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
